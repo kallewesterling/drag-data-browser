d097ae4a6d298aaf0ab77be1992f5528c9729253
Kalle Westerling <7298727+kallewesterling@users.noreply.github.com>
2021-08-31 14:44:03 -0300

    Afternoon updates

--- docs/includes/js/network-app-settings.js
+++ docs/includes/js/network-app-settings.js
@@ -1,11 +1,19 @@
 document.querySelector('#toggleFullscreen').addEventListener("click", (evt) => {
     if (d3.select('body').attr('data-fullscreen') === 'true') {
-        d3.select('body').style('left', '0px')
-        d3.select('body').style('width', 'inherit')
-        d3.select('body').attr('data-fullscreen', 'false')
+        d3.select('body').style('left', '0px');
+        d3.select('body').style('width', 'inherit');
+        d3.select('body').attr('data-fullscreen', 'false');
     } else {
-        d3.select('body').style('left', '-280px')
-        d3.select('body').style('width', 'calc(100% + 280px)')
-        d3.select('body').attr('data-fullscreen', 'true')
+        if (window.innerWidth < 1199.98) {
+            console.log('small window')
+            d3.select('body').style('left', '-4.5em');
+            d3.select('body').style('width', 'calc(100% + 4.5em)');
+            d3.select('body').attr('data-fullscreen', 'true');
+            return true;
+        }
+        d3.select('body').style('left', 'calc(-280px - 1.5em)');
+        d3.select('body').style('width', 'calc(100% + (280px + 1.5em))');
+        d3.select('body').attr('data-fullscreen', 'true');
+        return true;
     }
 })
